da2ffdde0fc709563e7b77838dc3200b9dfd0ac7
kyrylokhlopko <khlopko@gmail.com>
2024-01-18 22:55:27 +0100

    Initial setup of a server in Swift using Vapor

--- everyday.ts
+++ everyday.ts
@@ -1,109 +1,3 @@
-function createList() { const parent = document.getElementById('list');
-    if (!parent) {
-        return;
-    }
-    parent.innerHTML = '';
-    const items = load();
-    if (items.length === 0) {
-        parent.appendChild(noTasksElement());
-    }
-    items.forEach((item) => {
-        const div = document.createElement('div');
-        div.className = 'item' + (item.isDone ? ' faded' : '');
-        const divWrapper = document.createElement('div');
-        divWrapper.className = 'flex';
-        div.appendChild(divWrapper);
-        divWrapper.appendChild(doneButton(item));
-        divWrapper.appendChild(taskBody(item));
-        divWrapper.appendChild(removeButton(item));
-        parent.appendChild(div);
-    });
-}
-
-function doneButton(item: TaskItem): HTMLElement {
-    const done = document.createElement('button');
-    done.innerHTML = item.isDone ? 'undo' : 'done';
-    done.className = 'primary' + (item.isDone ? ' done' : '');
-    done.onclick = () => {
-        toggle(item.id);
-        createList();
-    };
-    return done;
-}
-
-function taskBody(item: TaskItem): HTMLElement {
-    const text = document.createElement('span');
-    text.innerHTML = item.name;
-    text.className = 'task-body ' + (item.isDone ? 'done' : '');
-    return text;
-}
-
-function removeButton(item: TaskItem): HTMLElement {
-    const remove = document.createElement('button');
-    remove.className = 'remove';
-    remove.innerHTML = 'remove';
-    remove.onclick = () => {
-        deleteItem(item.id);
-        createList();
-    };
-    return remove;
-}
-
-function noTasksElement(): HTMLElement {
-    const div = document.createElement('div');
-    div.className = 'item';
-    const text = document.createElement('span');
-    text.innerHTML = 'No tasks';
-    div.appendChild(text);
-    return div;
-}
-
-function add() {
-    const input = getInputElement();
-    create(input.value);
-    createList();
-}
-
-function addListener() {
-    const input = getInputElement();
-    if (!input) {
-        return;
-    }
-    input.addEventListener('keyup', function(event) {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            add();
-            input.value = '';
-        }
-    });
-}
-
-function getInputElement() {
-    return document.getElementById('task-input') as HTMLInputElement;
-}
-
-function onLoad() {
-    autoResetInitialConfig();
-    updateAutoResetButton();
-    refreshItemsIfNewDay();
-    recordVisit();
-}
-
-function updateAutoResetButton() {
-    const autoResetElement = document.getElementById('auto-reset');
-    if (!autoResetElement) {
-        return;
-    }
-    const existingChild = autoResetElement.firstChild;
-    if (existingChild) {
-        autoResetElement.removeChild(existingChild);
-    }
-    autoResetElement.className = autoResetEnabled() ? 'green' : 'red';
-    const textParent = document.createElement('b');
-    textParent.innerText = autoResetEnabled() ? 'on' : 'off';
-    autoResetElement.appendChild(textParent);
-}
-
 type TaskItem = {
     id: string,
     name: string,
@@ -197,6 +91,117 @@ function update(items: TaskItem[]) {
     window.localStorage.setItem('tasks', serializedItems);
 }
 
+// MARK: - UI
+
+function createList() { const parent = document.getElementById('list');
+    if (!parent) {
+        return;
+    }
+    parent.innerHTML = '';
+    const items = load();
+    if (items.length === 0) {
+        parent.appendChild(noTasksElement());
+    }
+    items.forEach((item) => {
+        const div = document.createElement('div');
+        div.className = 'item' + (item.isDone ? ' faded' : '');
+        const divWrapper = document.createElement('div');
+        divWrapper.className = 'flex';
+        div.appendChild(divWrapper);
+        divWrapper.appendChild(doneButton(item));
+        divWrapper.appendChild(taskBody(item));
+        divWrapper.appendChild(removeButton(item));
+        parent.appendChild(div);
+    });
+}
+
+function doneButton(item: TaskItem): HTMLElement {
+    const done = document.createElement('button');
+    done.innerHTML = item.isDone ? 'undo' : 'done';
+    done.className = 'primary' + (item.isDone ? ' done' : '');
+    done.onclick = () => {
+        toggle(item.id);
+        createList();
+    };
+    return done;
+}
+
+function taskBody(item: TaskItem): HTMLElement {
+    const text = document.createElement('span');
+    text.innerHTML = item.name;
+    text.className = 'task-body ' + (item.isDone ? 'done' : '');
+    return text;
+}
+
+function removeButton(item: TaskItem): HTMLElement {
+    const remove = document.createElement('button');
+    remove.className = 'remove';
+    remove.innerHTML = 'remove';
+    remove.onclick = () => {
+        deleteItem(item.id);
+        createList();
+    };
+    return remove;
+}
+
+function noTasksElement(): HTMLElement {
+    const div = document.createElement('div');
+    div.className = 'item';
+    const text = document.createElement('span');
+    text.innerHTML = 'No tasks';
+    div.appendChild(text);
+    return div;
+}
+
+function updateAutoResetButton() {
+    const autoResetElement = document.getElementById('auto-reset');
+    if (!autoResetElement) {
+        return;
+    }
+    const existingChild = autoResetElement.firstChild;
+    if (existingChild) {
+        autoResetElement.removeChild(existingChild);
+    }
+    autoResetElement.className = autoResetEnabled() ? 'green' : 'red';
+    const textParent = document.createElement('b');
+    textParent.innerText = autoResetEnabled() ? 'on' : 'off';
+    autoResetElement.appendChild(textParent);
+}
+
+function getInputElement() {
+    return document.getElementById('task-input') as HTMLInputElement;
+}
+
+// MARK: - Actions
+
+function add() {
+    const input = getInputElement();
+    create(input.value);
+    createList();
+}
+
+function addListener() {
+    const input = getInputElement();
+    if (!input) {
+        return;
+    }
+    input.addEventListener('keyup', function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            add();
+            input.value = '';
+        }
+    });
+}
+
+function onLoad() {
+    autoResetInitialConfig();
+    updateAutoResetButton();
+    refreshItemsIfNewDay();
+    recordVisit();
+}
+
 addListener();
 onLoad();
 createList();
+
