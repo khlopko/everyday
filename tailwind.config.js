40b4bca65e10055c6783ad833d232a0dfc1d3dc5
kyrylokhlopko <khlopko@gmail.com>
2024-01-20 15:59:25 +0100

    v13.3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
     content: ["./**/*.{html,js,ts}"],
     theme: {
+        screens: {
+            'sm': '240px',
+            'md': '320px',
+            'lg': '480px',
+            'xl': '640px',
+            '2xl': '768px',
+        },
         extend: {},
     },
     plugins: [],
